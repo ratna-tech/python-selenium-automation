0b79cfe683e7bf122f4c5b1910d45d1d08f63184
ratna-tech <ratnasinha012@gmail.com>
2021-02-09 13:45:03 -0500

    first commit

--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path=r"C:\Users\Ratna Sinha\Downloads\chromedriver_win32\Chromedriver.exe")
 driver.maximize_window()
 
 # open the url
